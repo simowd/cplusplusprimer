1ebf30e6d0cbfc07acc9a224f8ab45b810523dca
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-06-17 17:46:35 -0400

    Finished 2.5 to 2.11

---
+++ chapter2/ex2-10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+std::string global_str; // default, ""
+int global_int; // 0
+
+int main() {
+    int local_int; // uninitialized
+    std::string local_str; // default, ""
+    return 0;
+}
